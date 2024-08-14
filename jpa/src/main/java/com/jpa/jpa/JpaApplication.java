cd06ad7abf72b1aac84750350991b225d12a515c
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-08-14 15:23:35 -0500

    FORMAS DE HACER CONSULTAS , QUERYS PROPIEAS Y OPTIONAL

--- jpa/src/main/java/com/jpa/jpa/JpaApplication.java
+++ jpa/src/main/java/com/jpa/jpa/JpaApplication.java
@@ -1,6 +1,7 @@
 package com.jpa.jpa;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,7 @@ import com.jpa.jpa.respositories.PersonRepositoty;
 public class JpaApplication implements CommandLineRunner{
 
 	@Autowired // Traigo el personRepository y lo injecto como dependencia
-	private PersonRepositoty personRepositoty;
+	private PersonRepositoty personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
@@ -41,33 +42,146 @@ public class JpaApplication implements CommandLineRunner{
 		
 	}
 
-
-	@Transactional // Si algo sale mal detiene el proceso y elimina los cambios hechos
+	@Transactional
 	public void create() {
+		
 		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Ingrese el nombre");
+		System.out.println("Ingrese el nombre:");
 		String name = scanner.next();
-
-		System.out.println("Ingrese el apellido");
+		System.out.println("Ingrese el apellido:");
 		String lastname = scanner.next();
-
-		System.out.println("Ingrese el lenguaje de programaciòn");
-		String programminLanguage = scanner.next();
-
+		System.out.println("Ingrese el lenguaje de programacion:");
+		String programmingLanguage = scanner.next();
 		scanner.close();
 
-		Person person = new Person(null , name , lastname , programminLanguage);
+		Person person = new Person(null, name, lastname, programmingLanguage);
 
-		Person newPerson = personRepositoty.save(person);
+		Person personNew = personRepository.save(person);
+		System.out.println(personNew);
 
-		System.out.println(newPerson);
+		personRepository.findById(personNew.getId()).ifPresent(System.out::println);
 
-		personRepositoty.findById(newPerson.getId()).ifPresent(System.out::println);
+	}
+	@Transactional
+	public void update() {
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el id de la persona:");
+		Long id = scanner.nextLong();
+
+		Optional<Person> optionalPerson = personRepository.findById(id);
+
+		// optionalPerson.ifPresent(person -> {
+		if (optionalPerson.isPresent()) {
+			Person personDB = optionalPerson.orElseThrow();	
+
+			System.out.println(personDB);
+			System.out.println("Ingrese el lenguaje de programacion:");
+			String programmingLanguage = scanner.next();
+			personDB.setProgrammingLanguage(programmingLanguage);
+			Person personUpdated = personRepository.save(personDB);
+			System.out.println(personUpdated);
+		} else {
+			System.out.println("El usuario no esta presente! no existe!");
+		}
+
+		scanner.close();
+	}
+
+	@Transactional(readOnly = true)
+	public void queriesFunctionAggregation() {
 
 		
+		System.out.println("================== consulta con el total de registros de la tabla persona ==================");
+		Long count = personRepository.getTotalPerson();
+		System.out.println(count);
+		
+		System.out.println("================== consulta con el valor minimo del id ==================");
+		Long min = personRepository.getMinId();
+		System.out.println(min);
+		
+		System.out.println("================== consulta con el valor maximo del id");
+		Long max = personRepository.getMaxId();
+		System.out.println(max);
+		
+		System.out.println("================== consulta con el nombre y su largo ==================");
+		List<Object[]> regs = personRepository.getPersonNameLength();
+		regs.forEach(reg -> {
+			String name = (String) reg[0];
+			Integer length = (Integer) reg[1];
+			System.out.println("name=" + name + ", length=" + length);
+		});
+		
+		System.out.println("================== consulta con el nombre mas corto ==================");
+		Integer minLengthName = personRepository.getMinLengthName();
+		System.out.println(minLengthName);
+		
+		System.out.println("================== consulta con el nombre mas largo ==================");
+		Integer maxLengthName = personRepository.getMaxLengthName();
+		System.out.println(maxLengthName);
+
+		System.out.println("================== consultas resumen de funciones de agregacion min, max, sum, avg, count ==================");
+		Object[] resumeReg = (Object[]) personRepository.getResumeAggregationFunction();
+		System.out.println(
+			    "min=" + resumeReg[0] +
+				", max=" + resumeReg[1] +
+				", sum=" + resumeReg[2] +
+				", avg=" + resumeReg[3] +
+		        ", count=" + resumeReg[4]);
 	}
+
+	@Transactional(readOnly=true)
+	public void personalizedQueriesBetween() {
+		System.out.println("================== consultas por rangos ==================");
+		List<Person> persons = personRepository.findByIdBetweenOrderByNameAsc(2L, 5L);
+		persons.forEach(System.out::println);
+		
+		persons = personRepository.findByNameBetweenOrderByNameDescLastnameDesc("J", "Q");
+		persons.forEach(System.out::println);
+
+		persons = personRepository.findAllByOrderByNameAscLastnameDesc();
+		persons.forEach(System.out::println);
+	}
+
+	@Transactional(readOnly = true)
+	public void personalizedQueriesConcatUpperAndLowerCase() {
+		System.out.println("================== consulta nombres y apellidos de personas ==================");
+		List<String> names = personRepository.findAllFullNameConcat();
+		names.forEach(System.out::println);
+
+		System.out.println("================== consulta nombres y apellidos mayuscula ==================");
+		names = personRepository.findAllFullNameConcatUpper();
+		names.forEach(System.out::println);
+
+		System.out.println("================== consulta nombres y apellidos minuscula ==================");
+		names = personRepository.findAllFullNameConcatLower();
+		names.forEach(System.out::println);
+		System.out.println("================== consulta personalizada persona upper y lower case ==================");
+		List<Object[]> regs = personRepository.findAllPersonDataListCase();
+		regs.forEach(reg -> System.out.println("id="+ reg[0] + ", nombre=" + reg[1] + ", apellido=" + reg[2]+ ", lenguaje="+reg[3]));
+
+	}
+
+	@Transactional(readOnly = true)
+	public void personalizedQueriesDistinct() {
+		System.out.println("================== consultas con nombres de personas ==================");
+		List<String> names = personRepository.findAllNames();
+		names.forEach(System.out::println);
+
+		System.out.println("==================  consultas con nombres unicos de personas ==================");
+		names = personRepository.findAllNamesDistinct();
+		names.forEach(System.out::println);
+
+		System.out.println("================== consulta con lenguaje de programacion unicas ==================");
+		List<String> languages = personRepository.findAllProgrammingLanguageDistinct();
+		languages.forEach(System.out::println);
+
+		System.out.println("================== consulta con total de lenguajes de programacion unicas ==================");
+		Long totalLanguage = personRepository.findAllProgrammingLanguageDistinctCount();
+		System.out.println("total de lenguajes de programacion: " + totalLanguage);
+
+	}
+	
 
 
 
--- jpa/src/main/java/com/jpa/jpa/respositories/PersonRepositoty.java
+++ jpa/src/main/java/com/jpa/jpa/respositories/PersonRepositoty.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.jpa.jpa.entities.Person;
+import com.jpa.jpa.entities.PersonDto;
+
 import java.util.List;
+import java.util.Optional;
 
 
 
@@ -13,11 +16,103 @@ import java.util.List;
 
 public interface PersonRepositoty extends JpaRepository<Person, Long> {
 
+    // CONSULTA USANDO LENGUAJE JPA
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
-
-    // CREO MI PROPIA CONSULTA
+    // CREO MI PROPIA CONSULTA CON QUERY 
     @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    // NOMBRE DE LA CONSULTA Y VALOR QUE RETORNA 
     List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+
+
+    // RETORNA LA CONSULTA COMO UNA LISTA DE OBJETOS
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    // EL OPTIONAL ES PARA DECIR QUE POSIBLEMENTE SE RETORNEN NULOS Y NO USAR LOS NULL-EXCEPTIONS
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+    
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByNameContaining(String name);
+
+
+    @Query("select p from Person p where p.id in ?1")
+    public List<Person> getPersonsByIds(List<Long> ids);
+
+    @Query("select p.name, length(p.name) from Person p where length(p.name)=(select min(length(p.name)) from Person p)")
+    public List<Object[]> getShorterName();
+    
+    @Query("select p from Person p where p.id=(select max(p.id) from Person p)")
+    public Optional<Person> getLastRegistration();
+    //------------------- FUNCIONES AGREGADAS -----------------------------------
+    @Query("select min(p.id), max(p.id), sum(p.id), avg(length(p.name)), count(p.id) from Person p")
+    public Object getResumeAggregationFunction();
+    
+    @Query("select min(length(p.name)) from Person p")
+    public Integer getMinLengthName();
+    
+    @Query("select max(length(p.name)) from Person p")
+    public Integer getMaxLengthName();
+
+    @Query("select p.name, length(p.name) from Person p")
+    public List<Object[]> getPersonNameLength();
+
+    @Query("select count(p) from Person p")
+    Long getTotalPerson();
+
+    @Query("select min(p.id) from Person p")
+    Long getMinId();
+    
+    @Query("select max(p.id) from Person p")
+    Long getMaxId();
+    //------- QUIERIES USIN BETWEEN ---------------
+    List<Person> findByIdBetweenOrderByNameAsc(Long id1, Long id2);
+
+    List<Person> findByNameBetweenOrderByNameDescLastnameDesc(String name1, String name2);
+
+    @Query("select p from Person p where p.id between ?1 and ?2 order by p.name desc")
+    List<Person> findAllBetweenId(Long id1, Long id2);
+
+    @Query("select p from Person p where p.name between ?1 and ?2 order by p.name asc, p.lastname desc")
+    List<Person> findAllBetweenName(String c1, String c2);
+
+    List<Person> findAllByOrderByNameAscLastnameDesc();
+
+    //----- USANDO CONCATENACION, UPPER Y LOWER CASE ---------
+    @Query("select upper(p.name || ' ' || p.lastname) from Person p")
+    List<String> findAllFullNameConcatUpper();
+    
+    @Query("select lower(concat(p.name, ' ', p.lastname)) from Person p")
+    List<String> findAllFullNameConcatLower();
+
+    // @Query("select CONCAT(p.name, ' ', p.lastname) from Person p")
+    @Query("select p.name || ' ' || p.lastname from Person p")
+    List<String> findAllFullNameConcat();
+    @Query("select p.id, upper(p.name), lower(p.lastname), upper(p.programmingLanguage) from Person p")
+    List<Object[]> findAllPersonDataListCase();
+    //******** USING DISTINC -------------------
+    @Query("select p.name from Person p")
+    List<String> findAllNames();
+
+    @Query("select distinct(p.name) from Person p")
+    List<String> findAllNamesDistinct();
+    
+    @Query("select new com.andres.curso.springboot.jpa.springbootjpa.dto.PersonDto(p.name, p.lastname) from Person p")
+    List<PersonDto> findAllPersonDto();
+    
+    @Query("select new Person(p.name, p.lastname) from Person p")
+    List<Person> findAllObjectPersonPersonalized();
+
+    @Query("select count(distinct(p.programmingLanguage)) from Person p")
+    Long findAllProgrammingLanguageDistinctCount();
+    
+    @Query("select distinct(p.programmingLanguage) from Person p")
+    List<String> findAllProgrammingLanguageDistinct();
 
 }
