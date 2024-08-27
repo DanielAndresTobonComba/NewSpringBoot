f3174e5ddaa882eec545e01ba91fdda466ae922c
Daniel Andres Tobon Comba <106559688+DanielAndresTobonComba@users.noreply.github.com>
2024-08-27 16:57:44 -0500

    Add files via upload

---
+++ hexagonal-app/src/main/java/com/hexagonal/hexagonal_app/HexagonalAppApplication.java
@@ -0,0 +1,15 @@
+package com.hexagonal.hexagonal_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalAppApplication {
+
+
+	// MANEJO DE EXCEPCIONES
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalAppApplication.class, args);
+	}
+
+}
---
+++ hexagonal-app/src/main/java/com/hexagonal/hexagonal_app/infrastructure/ExceptionController/HandlerExceptionController.java
@@ -0,0 +1,53 @@
+package com.hexagonal.hexagonal_app.infrastructure.ExceptionController;
+
+import java.util.Date;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import com.hexagonal.hexagonal_app.infrastructure.Models.ErrorCustom;
+
+
+// version avanzada del controllerAdvice 
+
+@RestControllerAdvice
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ArithmeticException.class})
+
+    public ResponseEntity<ErrorCustom> divisionByZero(Exception ex) {
+
+        ErrorCustom error = new ErrorCustom(); // creo el objeto de la clase errorcustom
+        error.setDate(new Date()); // tomo la fecha del sistema
+        error.setError("Error divisiòn por cero"); // coloco el mensaje predeterminado
+        error.setMessage(ex.getMessage()); // obtengo el mensaje de la excepcion del error
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value()); // obtengo el codigo de estatus del error
+
+
+        // el repsonse entity necesita si o si un codigo de status
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+
+        // retorno la entidad completa y como cuerpo es el objeto error con los datos
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+
+    public ResponseEntity<ErrorCustom> notFoundEx(NoResourceFoundException ex) {
+
+        ErrorCustom error = new ErrorCustom(); 
+        error.setDate(new Date()); 
+        error.setError("Api rest no encontrado"); 
+        error.setMessage(ex.getMessage()); 
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value()); 
+
+
+        
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+
+}
